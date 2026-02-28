6ef19241d545b78d1dfc9140f16d501fa1884bfa
TwoPenniesScreen <twopenniesscreen@gmail.com>
2026-02-28 18:52:35 +0000

    Update teams.js

--- netlify/functions/teams.js
+++ netlify/functions/teams.js
@@ -1,1 +1,4 @@
-exports.handler = require("./scores.js").handler;
+// Compatibility alias: /.netlify/functions/teams
+// Returns the same as /scores so old URLs don't break.
+const scores = require("./scores.js");
+exports.handler = scores.handler;
