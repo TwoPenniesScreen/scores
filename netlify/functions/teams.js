7d836c5793abe808f484921341faebeaaaa12724
TwoPenniesScreen <twopenniesscreen@gmail.com>
2026-02-28 18:44:59 +0000

    Update teams.js

--- netlify/functions/teams.js
+++ netlify/functions/teams.js
@@ -1,8 +1,1 @@
-// Netlify Function: /.netlify/functions/teams
-// Compatibility alias: returns same as /scores
-
-const scores = require("./scores.js");
-
-exports.handler = async (event, context) => {
-  return scores.handler(event, context);
-};
+exports.handler = require("./scores.js").handler;
